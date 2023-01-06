0e2c753d768ce7e0e2ee8bac9d4bc9240a449cb1
dango <dango@iptp.vn>
2023-01-06 10:46:13 +0700

    Add function validateYTUrl

--- ytplayer_pkg/utils.py
+++ ytplayer_pkg/utils.py
@@ -9,3 +9,17 @@ min_duration_time = datetime.strptime(min_duration_time_str, TIME_FORMAT)
 def checkDuration(yt_vid):
     vid_duration = datetime.strptime(yt_vid.duration, TIME_FORMAT)
     return vid_duration < max_duration_time and vid_duration >= min_duration_time
+
+def validateYTUrl(text):
+    begin = text.index(" <")
+    begin = begin + 2
+    url = text[begin:]
+    end = url.index(">")
+    url = url[0: end]
+    print(">>/youtube ", begin, text, end, url)
+    # Check "|" in url 
+    if "|" in url:
+        end = url.index("|")
+        url = url[0: end]
+    return url
+    
